5c8252580b32af0a6cd438b51ddf22f1c06c4fdb
Chandrakesh Dhawal <dhawal.dkd@gmail.com>
2025-08-31 17:29:30 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// Toggle mobile menu
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+//typewriter
+const el = document.getElementById("typewriter");
+const text = "Web Developer";
+
+let i = 0;
+let deleting = false;
+
+function typeEffect() {
+  if (!deleting) {
+    // typing
+    el.textContent = text.slice(0, ++i);
+    if (i === text.length) {
+      deleting = true;
+      setTimeout(typeEffect, 2000); // 2 sec pause after complete
+      return;
+    }
+  } else {
+    // deleting
+    el.textContent = text.slice(0, --i);
+    if (i === 0) {
+      deleting = false;
+    }
+  }
+  setTimeout(typeEffect, deleting ? 80 : 100); // typing / deleting speed
+}
+
+typeEffect();
+
